223736fb98f72e9bdd7d1e9dcf75ed9a3d4e5461
devoluo <lgclgclgc234@163.com>
2016-05-23 18:11:40 +0800

    add node moudle

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
   var path = require('path');
   var app = express();
   var server = require('http').createServer(app);
+  var dir = 'dist';
+  var appName = 'my';
   if (process.env.NODE_ENV === 'dev') {
     require('./webpackdev.server')(app)
+    dir = 'app';
   }
-  app.use('/app', express.static(path.join(__dirname, 'app')));
+  app.use('/', express.static(path.join(__dirname, dir)));
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'app/views/index.html'));
+    res.sendFile(path.join(__dirname, dir + '/views/index.html'));
   });
   server.listen(8080, function() {
     console.log('listened 8080 port server run!');
