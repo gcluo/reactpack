89cee7ddaf118744e6914cffe85f8f0b7cebc134
罗广从 <luoguangcong@Home.local>
2016-06-07 14:56:02 +0800

    homepage static

--- app/scripts/components/common/header.js
+++ app/scripts/components/common/header.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
 const Header = () => (
-  <header>
+  <header className="header-top">
     <div className="header">
-      <span>BB,专业的新闻聚合平台</span>
+      <span className="header-link">BB,专业的新闻聚合平台</span>
       <form className="header-search-form">
+        <span className="header-search-button"></span>
         <input type="text" className="header-search"/>
       </form>
-      <button>首页</button>
-      <button>热点</button>
-      <div className="header-inline-block">
-        <img src="" alt="头像"/>
-        <span>
-          <a href=""></a>
-        </span>
+      <ul className="header-menu">
+        <li><a href="">首页</a></li>
+        <li><a href="">热点</a></li>
+      </ul>
+      <div className="header-profile">
+        <span>your</span>
+        <img className="avatar" src="http://dummyimage.com/25x25/000/fff"/>
       </div>
     </div>
   </header>
--- app/scripts/components/homepage/news.js
+++ app/scripts/components/homepage/news.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 const News = () => (
   <article className="news">
-    <header>最新动态</header>
-    <section>
-      <header>百度魏则西专题</header>
-      <ul>
+    <header className="news-header">最新动态</header>
+    <section className="news-section">
+      <header className="news-title">百度魏则西专题</header>
+      <ul className="news-list">
         <li><a href="">文章1</a></li>
         <li><a href="">文章2</a></li>
         <li><a href="">文章3</a></li>
         <li><a href="">文章4</a></li>
+      </ul>
+    </section>
+    <section className="news-section">
+      <header className="news-title">阿依代打</header>
+      <ul className="news-list">
+      </ul>
+    </section>
+    <section className="news-section">
+      <header className="news-title">雷阳之死</header>
+      <ul className="news-list">
       </ul>
     </section>
   </article>
